b218d90e9c1d148870f44cdb0b679346fe5e5a23
HASAN AYDINOGLU <hasan_aydng@hotmail.com>
2022-03-08 19:02:54 +0300

    kitap satisi eklendi

---
+++ kitap/kitap.js
@@ -0,0 +1,11 @@
+function satinAl(){
+
+    var element = document.getElementById("image");
+    element.style.backgroundColor="red"; 
+
+    var mesajelementi = document.getElementById("mesaj");
+    mesajelementi.innerHTML="<h1>kitap satin alindi</h1>";
+    
+    console.log("SATIN ALINDI");
+    
+    }
