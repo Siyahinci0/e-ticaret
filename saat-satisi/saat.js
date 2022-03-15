4a0169ba9eb79e2eb14866917111fb98d8cc451d
HASAN AYDINOGLU <hasan_aydng@hotmail.com>
2022-03-15 16:34:28 +0300

    saatler olusturuldu

---
+++ saat-satisi/saat.js
@@ -0,0 +1,47 @@
+function mavisaat(){
+
+    var element = document.getElementById("saat");
+    element.src = "mavi.jpg"; 
+
+    
+
+    var saatcesidi = document.getElementById("saatcesidi");
+    saatcesidi.innerHTML="<h1>mavi saat</h1>";
+
+    var saatfiyati = document.getElementById("fiyat");
+    saatfiyati.innerHTML="<h1>fiyat: 120 TL</h1>";
+    
+   
+    
+    }
+
+    function beyazsaat(){
+
+        var element = document.getElementById("saat");
+        element.src = "beyaz.jpg"; 
+    
+        
+    
+        var saatcesidi = document.getElementById("saatcesidi");
+        saatcesidi.innerHTML="<h1>beyaz saat</h1>";
+    
+        var saatfiyati = document.getElementById("fiyat");
+        saatfiyati.innerHTML="<h1>fiyat: 150 TL</h1>";
+
+    }
+        
+
+    function siyahsaat(){
+
+        var element = document.getElementById("saat");
+        element.src = "siyah.jpg"; 
+    
+        
+    
+        var saatcesidi = document.getElementById("saatcesidi");
+        saatcesidi.innerHTML="<h1>siyah saat</h1>";
+    
+        var saatfiyati = document.getElementById("fiyat");
+        saatfiyati.innerHTML="<h1>fiyat: 180 TL</h1>";
+
+    }
